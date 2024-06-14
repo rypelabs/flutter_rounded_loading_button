8cba1753e4dffd65e29d2d7a5b31e875e85a075f
Alex Baker <iamalexbaker@gmail.com>
2024-06-14 17:43:04 +0700

    rxdart update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -40,89 +40,102 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     _btnController1.stateStream.listen((value) {
       print(value);
-
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Rounded Loading Button Demo'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RoundedLoadingButton(
-                successIcon: Icons.cloud,
-                failedIcon: Icons.cottage,
-                child: Text('Tap me!', style: TextStyle(color: Colors.white)),
-                controller: _btnController1,
-                onPressed: () => _doSomething(_btnController1),
+      appBar: AppBar(
+        title: Text('Rounded Loading Button Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RoundedLoadingButton(
+              successIcon: Icons.cloud,
+              failedIcon: Icons.cottage,
+              child: Text('Tap me!', style: TextStyle(color: Colors.white)),
+              controller: _btnController1,
+              onPressed: () => _doSomething(_btnController1),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            RoundedLoadingButton(
+              color: Colors.amber,
+              successColor: Colors.amber,
+              controller: _btnController2,
+              onPressed: () => _doSomething(_btnController2),
+              valueColor: Colors.black,
+              borderRadius: 10,
+              child: Text(
+                '''
+Tap me i have a huge text''',
+                style: TextStyle(color: Colors.white),
               ),
-              SizedBox(
-                height: 50,
-              ),
-              RoundedLoadingButton(
-                color: Colors.amber,
-                successColor: Colors.amber,
-                controller: _btnController2,
-                onPressed: () => _doSomething(_btnController2),
-                valueColor: Colors.black,
-                borderRadius: 10,
-                child: Text('''
-Tap me i have a huge text''', style: TextStyle(color: Colors.white),),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              OutlinedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),),),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            OutlinedButton(
+              style: ButtonStyle(
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
                   ),
-                  onPressed: () {
-                    _btnController1.reset();
-                    _btnController2.reset();
-                  },
-                  child: Text('Reset'),),
-              SizedBox(
-                height: 20,
-              ),
-              OutlinedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),),),
                 ),
-                onPressed: () {
-                  _btnController1.error();
-                  _btnController2.error();
-                },
-                child: Text('Error'),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              OutlinedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),),),
+              onPressed: () {
+                _btnController1.reset();
+                _btnController2.reset();
+              },
+              child: Text('Reset'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+              style: ButtonStyle(
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
                 ),
-                onPressed: () {
-                  // _btnController1.success();
-                  // _btnController2.success();
-                  // _btnController1
-                  print(_btnController1.currentState);
-                },
-                child: Text('Success'),
               ),
-            ],
-          ),
-        ),);
+              onPressed: () {
+                _btnController1.error();
+                _btnController2.error();
+              },
+              child: Text('Error'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+              style: ButtonStyle(
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                // _btnController1.success();
+                // _btnController2.success();
+                // _btnController1
+                print(_btnController1.currentState);
+              },
+              child: Text('Success'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
